d8bb1ebca35c1842bd9d98430a599e0632861a8d
Nick Singizwa <singizwanick19@gmail.com>
2026-03-26 14:56:51 +0200

    change base api

--- ps-access-tracker-backend/server.js
+++ ps-access-tracker-backend/server.js
@@ -10,7 +10,9 @@ const Service = require("./models/Service");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://ps-access-tracker.vercel.app"]
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
